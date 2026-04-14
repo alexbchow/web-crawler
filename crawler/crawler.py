a32efc011b5af43135b52c1336615c6fe03ec718
alexbchow <alexbchow@berkeley.edu>
2026-04-14 12:36:52 -0700

    base crawler implementation

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -21,6 +21,7 @@ The interesting engineering is in what happens around that loop:
 from crawler.fetcher import fetch
 from crawler.frontier import Frontier
 from crawler.parser import extract_links
+from requests import Session
 
 
 class Crawler:
@@ -32,8 +33,24 @@ class Crawler:
             seed_url: The starting URL for the crawl.
             max_pages: Stop after crawling this many pages.
         """
-        raise NotImplementedError("Implement this last.")
+        self.max_pages = max_pages
+        self.seed_url = seed_url
+        self.frontier = Frontier()
+        self.session = Session()
 
     def run(self) -> None:
         """Start the crawl loop and run until completion."""
-        raise NotImplementedError
+        self.frontier.add(self.seed_url)
+        pages_crawled = 0
+        while not self.frontier.is_empty() and pages_crawled != self.max_pages:
+          url = self.frontier.next()
+          pages_crawled+=1
+          try: 
+            html = fetch(url, self.session)
+          except Exception:
+             print(f"Failed {url}: {Exception}")
+             continue
+          links = extract_links(html, url)
+          for link in links:
+              self.frontier.add(link)
+
