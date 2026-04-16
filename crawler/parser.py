ba79272b47cc34cd723ab4243718d01ab29663de
alexbchow <alexbchow@berkeley.edu>
2026-04-16 16:39:47 -0700

    implemented canonical link extraction, added timeouts for tests

--- crawler/parser.py
+++ crawler/parser.py
@@ -111,6 +111,9 @@ def extract_links(html: str, base_url: str) -> list[str]:
     """
     urls = []
     soup = BeautifulSoup(html, "lxml")
+    canonical = soup.find("link", rel = "canonical")
+    if canonical and canonical.get("href"):
+        return normalize(canonical["href"])
     for link in soup.find_all("a", href=True):  # find all a tags in html
         if "nofollow" in link.get("rel", []):
             continue
